e0a76f221dfeaa10194bc6ad5c46bae8531aacb9
PauBC30 <pablanco@vlacademy.com>
2025-02-05 18:15:37 -0600

    Practica Calculadora

--- JS/PagesJS/Practica_Calculadora.js
+++ JS/PagesJS/Practica_Calculadora.js
@@ -1,9 +1,30 @@
 
 console.log(`Funcionando Archivo: Practica_Calculadora`);
 
- function enviarDatos() {
+function enviarDatos() {
     let nombre = document.getElementById("nombre").value;
     let edad = document.getElementById("edad").value;
+
+    //mostrar
+    //alert, console.log, inner
+    //cl snippet
+    console.log("Mostrar Nombre : " + nombre);
+    console.log("Mostrar Edad : " + edad);
+
+}
+
+
+function realizarSuma() {
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+
+    let suma = parseInt(numero1) + parseInt(numero2);
+
+    document.getElementById('resultado').innerText = suma
+
+    console.log("Suma: " + suma);
+
+
 
 }
 
