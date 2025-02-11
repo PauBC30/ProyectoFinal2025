753b9252e567a3ee03f72322748d0cc48fe6a7af
PauBC30 <pablanco@vlacademy.com>
2025-02-10 20:15:58 -0600

    Laboratorio 3

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -1,3 +1,37 @@
 console.log('Funcionando Archivo: API_Listado_Clientes');
 
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
+
+// Datos locales simulando la API
+let Datos_Locales = []
+
+
+let DatosFiltrados = []
+let contenedor = document.getElementById('tbody');
+
+
+DatosFiltrados = Datos_Locales
+
+
+//aela
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarDatos()
+
+});
+
+
+function consultarDatos() {
+    fetch(Api_Usuarios)
+        
+        .then(response => response.json())
+        .then(result => {
+            // custom error
+            console.log(result)
+        })
+        .catch(error => {
+            // common error
+            return null;
+        });
+}
+
